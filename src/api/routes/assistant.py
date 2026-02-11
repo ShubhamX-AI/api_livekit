554067bfc8fe0f35967cb63650d4f48f23252d16
Shubham Halder <shubhamworkonly@gmail.com>
2026-02-11 05:00:14 +0000

    added proper logging to auth and assitant.py

--- src/api/routes/assistant.py
+++ src/api/routes/assistant.py
@@ -3,9 +3,11 @@ from src.api.models.api_schemas import CreateAssistantRequest
 from src.api.models.response_models import apiResponse
 from src.core.db.db_schemas import Assistant, APIKey
 from src.api.dependencies import get_current_user
+from src.core.logger import logger, setup_logging
 import uuid
 
 router = APIRouter()
+setup_logging()
 
 # Create new assistant
 @router.post("/create")
@@ -13,6 +15,7 @@ async def create_assistant(
     request: CreateAssistantRequest,
     current_user: APIKey = Depends(get_current_user)
 ):
+    logger.info(f"Received request to create assistant")
     # Generate unique assistant ID
     assistant_id = str(uuid.uuid4())
     
@@ -20,6 +23,7 @@ async def create_assistant(
     assistant_data = request.model_dump()
     
     try:
+        logger.info(f"Inserting assistant into database")
         # Create database document
         new_assistant = Assistant(
             assistant_id=assistant_id,
@@ -27,8 +31,10 @@ async def create_assistant(
         )
         await new_assistant.insert()
     except Exception as e:
+        logger.error(f"Failed to create assistant: {e}")
         raise HTTPException(status_code=400, detail=f"Failed to create assistant: {e}")
     
+    logger.info(f"Assistant created successfully: {assistant_id}")
     return apiResponse(
         success=True,
         message="Assistant created successfully",
--- src/api/routes/auth.py
+++ src/api/routes/auth.py
@@ -3,13 +3,16 @@ from src.api.models.api_schemas import CreateApiKeyRequest
 from src.api.models.response_models import apiResponse
 from src.core.db.db_schemas import APIKey
 from src.api.dependencies import get_current_user
+from src.core.logger import logger,setup_logging
 import secrets
 
 router = APIRouter()
+setup_logging()
 
 # Create api key
 @router.post("/create-key")
 async def create_api_key(request: CreateApiKeyRequest):
+    logger.info(f"Received request to create API key")
     # check if user already exists
     existing_user = await APIKey.find_one(APIKey.user_email == request.user_email)
     if existing_user:
@@ -19,12 +22,15 @@ async def create_api_key(request: CreateApiKeyRequest):
     api_key = "lvk_" + secrets.token_urlsafe(32)
 
     try:
+        logger.info(f"Inserting API key into database")
         # create new user
         user = APIKey(user_name=request.user_name,org_name=request.org_name,user_email=request.user_email,api_key=api_key)
         await user.insert()
     except Exception as e:
+        logger.error(f"Failed to create API key: {e}")
         raise HTTPException(status_code=500, detail=str(e)) 
     
+    logger.info(f"API key created successfully")
     return apiResponse(
         success=True,
         message="API key created successfully, Store it securely",
@@ -35,6 +41,7 @@ async def create_api_key(request: CreateApiKeyRequest):
 # Check key details
 @router.get("/check-key")
 async def check_api_key(current_user: APIKey = Depends(get_current_user)):
+    logger.info(f"Checking API key for user: {current_user.user_email}")
     return apiResponse(
         success=True,
         message="API key is valid",
